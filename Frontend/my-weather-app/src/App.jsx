140302fd8bf4aa72cd4b5f73f7cfdeccc25eaa5c
AbhishekGaikwad45 <gaikwadabhi246@gmail.com>
2025-06-23 19:38:33 +0530

    first commit

---
+++ Frontend/my-weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import WeatherNow from './assets/Component/WeatherNow'
+import '../src/index.css';
+
+
+function App() {
+  return (
+    <div>
+      <WeatherNow/>
+      
+    </div>
+  )
+}
+
+export default App
